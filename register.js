cb6f40bcf9166cf489fa760e0ce22d83495c27ab
glinfo <glinfo@dddp-gl.org>
2026-02-25 17:42:20 +0900

    2026 2 25 d

---
+++ register.js
@@ -0,0 +1,3 @@
+const areaCodeX1 ='jp';
+const areaNameX1 ='Japan';
+const registerDay ='2026/2/25';
---
+++ survey.js
@@ -0,0 +1,5 @@
+const account = 'dddp-jp';
+const meetingCode = 'dddp-jp-----y2025-a100-p01';
+const surveyDay = '2026/2/25'; 
+const areaCode ='jp';
+const options = [3,6];
